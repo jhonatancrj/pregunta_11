670be23cb17454a6c086525a54f1e164fdfb22f5
jhonatancrj <jcorderor@fcpn.edu.bo>
2024-05-20 13:03:11 -0400

    Se agrega el Proyecto

---
+++ CalculadoraPostfija/CalculadoraPostfija/CalculadoraPostfija.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraPostfija
+{
+    class CalculadoraPostfija
+    {
+        public double EvaluarPostfija(string expresion)
+        {
+            Stack<double> pila = new Stack<double>();
+            string[] tokens = expresion.Split(' ');
+
+            foreach (string token in tokens)
+            {
+                if (double.TryParse(token, out double numero))
+                {
+                    pila.Push(numero);
+                }
+                else
+                {
+                    double b = pila.Pop();
+                    double a = pila.Pop();
+                    double resultado = 0;
+
+                    switch (token)
+                    {
+                        case "+":
+                            resultado = a + b;
+                            break;
+                        case "-":
+                            resultado = a - b;
+                            break;
+                        case "*":
+                            resultado = a * b;
+                            break;
+                        case "/":
+                            if (b != 0)
+                            {
+                                resultado = a / b;
+                            }
+                            else
+                            {
+                                throw new DivideByZeroException("División por cero.");
+                            }
+                            break;
+                        default:
+                            throw new InvalidOperationException("Operador no válido.");
+                    }
+                    pila.Push(resultado);
+                }
+            }
+
+            return pila.Pop();
+        }
+    }
+}
---
+++ CalculadoraPostfija/CalculadoraPostfija/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculadoraPostfija
+{
+    public partial class Form1 : Form
+    {
+        private CalculadoraPostfija calculadora;
+
+        public Form1()
+        {
+            InitializeComponent();
+            calculadora = new CalculadoraPostfija();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string expresion = textBox1.Text;
+                double resultado = calculadora.EvaluarPostfija(expresion);
+                textBox2.Text = resultado.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
